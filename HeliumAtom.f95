f190c615ca9d5a6c2c685d5d63464647f8460f4e
LordMomo <henriquetortura.ht@gmail.com>
2020-09-20 11:24:02 -0300

    Adequações e testes Poisson/Verlet

--- HeliumAtom.f95
+++ HeliumAtom.f95
@@ -6,12 +6,13 @@ program HeliumAtom
 
     integer i
     integer, parameter :: r0 = 0, r_max = 20, int_max = 100
-    real (kind = 8) , parameter :: h = 0.00004 ! Parâmetro da discretização
-    integer, parameter :: n = int((r_max-r0)/h) ! Número de intervalos
-    real (kind = 8) , dimension(1:n) :: r, Potential, u
+    real (kind = 8) , parameter :: h = 0.0004 ! Discretization
+    integer, parameter :: n = int((r_max-r0)/h) ! Number of steps
+
+    real (kind = 8) , dimension(1:n) :: r, Potential, u, Potential_U
+
     real (kind = 8)  :: EigenValue_min, EigenValue_max, E_aux, tolerance
 
-    ! Inicializando as posições e o Potencial
     do i=1, n
         r(i) = r0 + h*i
         Potential(i) = -1/r(i)
@@ -26,5 +27,14 @@ program HeliumAtom
     EigenValue_max = 0
 
     call KohnSham1D(u, Potential, h, int_max, EigenValue_min, EigenValue_max, tolerance)
+
+    call Poisson(r, u, Potential_U, h)
+
+    ! To plot Potential U
+    open(1, file='Potential_U.dat', status='replace')
+        do i=1, n
+            write(1,*) r(i), Potential_U(i)
+        end do
+    close(1)
 
 end program
