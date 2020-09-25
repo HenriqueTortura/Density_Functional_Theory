53546b01031dbec001a300c6c6bd2ecc08704317
LordMomo <henriquetortura.ht@gmail.com>
2020-09-25 18:24:28 -0300

    Plot 2 Hidrogênio

--- HeliumAtom/Plotting.py
+++ HeliumAtom/Plotting.py
@@ -10,39 +10,59 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+#%%
+if 1:
+    r, u = np.genfromtxt('u.dat', unpack = 'True', skip_footer=470000)
+    prob = u**2/(2*np.pi)
+    
+    theta = np.linspace(0,2*np.pi,50)
+    A, B = np.meshgrid(theta, r)
+    c = np.zeros(A.shape)
+    
+    T, probabilidade_2D = np.meshgrid(theta, prob)
+
+#%%
+    figure, ax = plt.subplots(1, subplot_kw=dict(projection='polar'))
+    figure.set_size_inches((16,16))
+    plot = ax.contourf(theta, r, probabilidade_2D, 100, cmap=plt.cm.inferno)
+    plt.colorbar(plot, ax=ax)
+    #ax.grid(False)
+    plt.savefig('hydrogen2D.png', dpi=200, bbox_inches='tight')
+
 #%% Sweep
-plot_all = 1
-plot_energy_levels = 1
-
-eigenvalues, u0s = np.genfromtxt('u0s.dat', unpack = 'True')
-
-plt.figure(figsize=(16,9))
-plt.title('Boundary Term vs Eigenvalue - '
-          +str(np.size(eigenvalues))+' points', fontsize=20)
-plt.plot(eigenvalues, u0s)
-plt.xlabel('Eigenvalue', fontsize=18)
-plt.ylabel('u(0)', fontsize=18)
-plt.grid()
-
-spec='global'
-
-if not plot_all:
-    #Plot energy levels
-    if plot_energy_levels:
-        plt.xlim(-0.6,0)
-        plt.ylim(-0.25,0.25)
-        spec = 'bound_levels'
-        
-        for i in range(1,5):
-            plt.vlines(-0.5/i**2, -0.2, 0.2,
-                       color='r', label='n='+str(i))
-            plt.text(-0.5/i**2, 0.2, 'n='+str(i),
-                     horizontalalignment='center', fontsize=14,
-                     bbox={'facecolor': 'red', 'alpha': 1, 'pad': 5})
-    else:
-        plt.xlim(0,5)
-        plt.ylim(-0.15,0.15)
-        spec='unknown'
-
-plt.tight_layout()
-plt.savefig(dpi = 200,fname = 'BoundaryTerm_vs_Eigenvalue--'+spec+'.png')
+if 0:
+    plot_all = 1
+    plot_energy_levels = 1
+    
+    eigenvalues, u0s = np.genfromtxt('u0s.dat', unpack = 'True')
+    
+    plt.figure(figsize=(16,9))
+    plt.title('Boundary Term vs Eigenvalue - '
+              +str(np.size(eigenvalues))+' points', fontsize=20)
+    plt.plot(eigenvalues, u0s)
+    plt.xlabel('Eigenvalue', fontsize=18)
+    plt.ylabel('u(0)', fontsize=18)
+    plt.grid()
+    
+    spec='global'
+    
+    if not plot_all:
+        #Plot energy levels
+        if plot_energy_levels:
+            plt.xlim(-0.6,0)
+            plt.ylim(-0.25,0.25)
+            spec = 'bound_levels'
+            
+            for i in range(1,5):
+                plt.vlines(-0.5/i**2, -0.2, 0.2,
+                           color='r', label='n='+str(i))
+                plt.text(-0.5/i**2, 0.2, 'n='+str(i),
+                         horizontalalignment='center', fontsize=14,
+                         bbox={'facecolor': 'red', 'alpha': 1, 'pad': 5})
+        else:
+            plt.xlim(0,5)
+            plt.ylim(-0.15,0.15)
+            spec='unknown'
+    
+    plt.tight_layout()
+    plt.savefig(dpi = 200,fname = 'BoundaryTerm_vs_Eigenvalue--'+spec+'.png')
