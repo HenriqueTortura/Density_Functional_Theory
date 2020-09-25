b0f296086e8db3eb8d74a69ca4e37c99f4fba49a
LordMomo <henriquetortura.ht@gmail.com>
2020-09-25 11:42:35 -0300

    Adequações e inclusão da varredura

--- HeliumAtom/util.f95
+++ HeliumAtom/util.f95
@@ -16,7 +16,7 @@ contains
     !   u: Radial wave function u(r) = r*\psi(r);
     !   EigenValue: Eigenvalue of function u(r) satisfying boundary conditions.
 
-        integer i, j, int_max
+        integer i, int_max
         real (kind = 8), dimension(:) :: Potential, u, r
         integer :: n
         real (kind = 8) :: EigenValue_min, EigenValue_max, EigenValue, E_aux, h, tolerance
@@ -111,20 +111,85 @@ contains
         a = (1 - Potential_U(size(Potential_U))) / r(size(r))
         Potential_U = Potential_U + a*r
 
-        contains
-            function Verlet(Potential_U, F, h, n)
-                integer :: i, n
-                real (kind = 8), dimension(1:n) :: F, Potential_U, Verlet
-                real (kind = 8) :: h
+    contains
+        function Verlet(Potential_U, F, h, n)
+            integer :: i, n
+            real (kind = 8), dimension(1:n) :: F, Potential_U, Verlet
+            real (kind = 8) :: h
 
-                do i=3, n
-                    Potential_U(i) = 2*Potential_U(i-1) - Potential_U(i-2) +h**2*F(i-1)
-                end do
+            do i=3, n
+                Potential_U(i) = 2*Potential_U(i-1) - Potential_U(i-2) +h**2*F(i-1)
+            end do
 
-                Verlet = Potential_U
+            Verlet = Potential_U
 
-            end function Verlet
+        end function Verlet
 
-        end subroutine Poisson
+    end subroutine Poisson
+
+!! Fooling around
+    subroutine KohnShamSweep(r, u, Potential, h, EigenValues, u0s)
+
+        integer i
+        real (kind = 8), dimension(:) :: Potential, u, r
+        real (kind = 8), dimension(:) :: EigenValues, u0s
+        integer :: n, n_eigenvalues
+        real (kind = 8) :: h
+
+        ! Arrays size and interation counter
+        n = size(u)
+        n_eigenvalues = size(EigenValues)
+        i = 1
+
+        ! Initial wave function guess
+        u(n) = r(n)*exp(-r(n))
+        u(n-1) = r(n-1)*exp(-r(n-1))
+
+        ! Main loop for finding the Kohn-Sham eigenvalue
+        do while (i <= n_eigenvalues)
+
+            ! Integrating wave function via Numerov
+            u = Numerov(h, n, u, -2*(EigenValues(i)-Potential))
+
+            ! Normalizing u
+            u = u/sqrt(sum(u*u*h))
+            u0s(i) = u(1)
+
+            print *,"Eigen Value tested:", EigenValues(i)
+            print *,"Boundary term:", u(1)
+            print *,"*-----------------------*"
+
+            i = i + 1
+
+        end do
+
+    contains
+
+        function Numerov(h, n, u, f)
+        ! Applies Numerov's method for the wave function.
+        ! Parameters
+        !   h: Discretization;
+        !   n: Number of steps, i.e., size of array;
+        !   (part of) u: Two guesses at boundary of wave function on the form u(r) = r*\psi(r);
+        !   f: f function on Numerov's method.
+        ! Returns
+        !   u: Integrated wave function.
+
+            integer :: i, n
+            real (kind = 8) :: h, a, b, c
+            real (kind = 8), dimension(1:n) :: u, f, Numerov
+
+            do i=2, n-1
+                a = 2*(1 + 5*(h**2)*f(n-i+1)/12)
+                b = (1 - (h**2)*f(n-i+2)/12)
+                c = (1 - (h**2)*f(n-i)/12)
+                u(n-i) = (a*u(n-i+1) - b*u(n-i+2)) / c
+            end do
+
+            Numerov = u
+
+        end function Numerov
+
+    end subroutine KohnShamSweep
 
 end module util
