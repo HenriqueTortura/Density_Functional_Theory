e3e003ce92beaefee8f19c9b92bfb02e0e912357
LordMomo <henriquetortura.ht@gmail.com>
2020-10-23 19:19:57 -0300

    Problema 5.1-c Thijssen resolvido, código organizado

--- HeliumAtom/HeliumAtom.f95
+++ HeliumAtom/HeliumAtom.f95
@@ -1,67 +1,34 @@
-program HeliumAtom
+program Teste
 
     use util
 
     implicit none
 
     ! Setting main parameters
-    integer, parameter :: r0 = 0, r_max = 30, int_max_KS = 100, int_max = 100 ! Radial and iterations limits
-    real (kind = 8) , parameter :: pi = 3.141592653589793, h = 0.0001, & ! Discretization
-    &Eigenvalue_tol = 0.001, u0_tol= 0.001, self_tol=0.001 ! Tolerances
+    real (kind = 8), parameter, dimension(2) :: r_range = (/ 0., 30./) , Eigenvalue_Range = (/ -5., 0./)
 
-    real (kind = 8), dimension(2) :: E_Range_aux, Eigenvalue_Range = (/ -5., 0./) ! Eigenvalue range
+    integer, parameter :: KS_int_max = 100, SelfCons_int_max = 100
 
-    integer, parameter :: n = int((r_max-r0)/h) ! Number of steps
+    real (kind = 8), parameter :: Eigenvalue_tol = 0.001, u0_tol = 0.001, SelfCons_tol = 0.001
 
-    integer i, j
-    real (kind = 8) , dimension(1:n) :: r, u, Ext_Potential, Hartree, Exchange, Potential_U
-    real (kind = 8)  :: Eigenvalue, Eigenvalue_aux, Energy
+    logical, parameter :: Uniform = .FALSE.
+
+    real (kind = 8), parameter :: h = 0.0001
+
+    integer, parameter :: j_max = 200000
+    real (kind = 8), parameter :: delta = 0.0001
 
     real :: start, finish
 
+    !---------------------------------------------------------------------------------------!
+
     call cpu_time(start)
 
-    ! Initializing radial coordinates and potentials
-    do i=1, n
-        r(i) = r0 + h*i
-        Ext_Potential(i) = -2/r(i)
-        Hartree(i) = 0
-        Exchange(i) = 0
-    end do
-
-    Eigenvalue_aux = 0
-    Eigenvalue = Eigenvalue_aux + 2*self_tol
-
-    i = 1
-
-    do i=1, int_max
-
-        print *,'**************************'
-        print *,'**************************'
-        print *,"Iteration: ",i
-
-        E_Range_aux = Eigenvalue_Range
-        Eigenvalue_aux = Eigenvalue
-
-        call KohnSham1D(r, u, Ext_Potential+Hartree+Exchange, E_Range_aux, Eigenvalue,&
-        &int_max_KS, Eigenvalue_tol, u0_tol, n, h, 0d8, 0d8, .TRUE.)
-
-        if (abs(Eigenvalue-Eigenvalue_aux)>=self_tol) then
-            call Poisson(r, u, Potential_U, n, h, 0d8, 0d8, .TRUE.)
-
-            Hartree = 2 * Potential_U / r
-
-            Exchange = -((3./2.)*(u/(pi * r))**2.)**(1.0/3.0)
-        else
-            exit
-        end if
-
-    end do
-
-    Energy = 2. * Eigenvalue - sum(Hartree*(u**2.)*h) - (1./2.)*sum(Exchange*(u**2.)*h)
-    print *,"Energy: ",Energy
+    call HeliumAtom(r_range, Eigenvalue_range, SelfCons_int_max, KS_int_max,&
+    &Eigenvalue_tol, u0_tol, SelfCons_tol, Uniform, h, j_max, delta)
 
     call cpu_time(finish)
     print *,"CPU time = ",finish-start,"s"
 
 end program
+
--- HeliumAtom/util.f95
+++ HeliumAtom/util.f95
@@ -4,6 +4,85 @@ module util
 
 contains
 
+    subroutine HeliumAtom(r_range, Eigenvalue_range, SelfCons_int_max, KS_int_max,&
+    &Eigenvalue_tol, u0_tol, SelfCons_tol, Uniform, h, j_max, delta)
+
+    integer :: n, j_max, i, j, KS_int_max, SelfCons_int_max, AllocateStatus
+    real (kind = 8) , parameter :: pi = 3.141592653589793
+
+    real (kind = 8), dimension(2) :: r_range, Eigenvalue_Range, E_Range_aux
+    real (kind = 8)  :: Eigenvalue, Eigenvalue_aux, Energy,&
+    &Eigenvalue_tol, u0_tol, SelfCons_tol, h, delta, rp
+
+    real (kind = 8), dimension(:), allocatable :: r, u, Ext_Potential, Hartree, Exchange, Potential_U, j_array
+
+    logical :: Uniform
+
+    if (Uniform) then
+        n = int((r_range(2)-r_range(1))/h)
+    else
+        rp = r_range(2)/(exp(j_max*delta)-1)
+        n = j_max
+    end if
+
+    allocate(r(n), u(n), Ext_Potential(n), Hartree(n), Exchange(n), Potential_U(n), j_array(N), stat = AllocateStatus)
+    if (AllocateStatus /= 0) stop "*** Not enough memory ***"
+
+    ! Initializing radial coordinates and potentials
+    do i=1, n
+
+        j_array(i) = i
+
+        if (Uniform) then
+            r(i) = r_range(1) + h*i
+        else
+            r(i) = rp*(exp(i*delta)-1)
+        end if
+
+        Ext_Potential(i) = -2/r(i)
+        Hartree(i) = 0
+        Exchange(i) = 0
+
+    end do
+
+    Eigenvalue_aux = 0
+    Eigenvalue = Eigenvalue_aux + 2*SelfCons_tol
+
+    do i=1, SelfCons_int_max
+
+        print *,'**************************'
+        print *,'**************************'
+        print *,"Iteration: ",i
+
+        E_Range_aux = Eigenvalue_Range
+        Eigenvalue_aux = Eigenvalue
+
+        call KohnSham1D(r, u, Ext_Potential+Hartree+Exchange, E_Range_aux, Eigenvalue,&
+        &KS_int_max, Eigenvalue_tol, u0_tol, n, h, rp, delta, Uniform)
+
+        if (abs(Eigenvalue-Eigenvalue_aux)>=SelfCons_tol) then
+            call Poisson(r, u, Potential_U, n, h, rp, delta, Uniform)
+
+            Hartree = 2 * Potential_U / r
+
+            Exchange = -((3./2.)*(u/(pi * r))**2.)**(1.0/3.0)
+        else
+            exit
+        end if
+
+    end do
+
+    if (Uniform) then
+        Energy = 2. * Eigenvalue - sum(Hartree*(u**2.)*h) - (1./2.)*sum(Exchange*(u**2.)*h)
+    else
+        Energy = 2. * Eigenvalue - rp*delta*sum(Hartree*(u**2.)*(exp(j_array*delta)-1))&
+        & - rp*delta*(1./2.)*sum(Exchange*(u**2.)*(exp(j_array*delta)-1))
+    end if
+
+    print *,"Energy: ",Energy
+
+    end subroutine HeliumAtom
+
     subroutine KohnSham1D(r, u, Potential, Eigenvalue_Range, EigenValue,&
     &int_max, eigenvalue_tol, u0_tol, n, h, rp, delta, Uniform)
     ! Routine to solve a one dimentional Kohn-Sham (Schrödinger) equation.
