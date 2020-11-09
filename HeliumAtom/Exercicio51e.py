3806e42e9e8337f16a72f23a711005e21928b1e0
LordMomo <henriquetortura.ht@gmail.com>
2020-11-09 13:55:52 -0300

    Exercício 5.1-e: Não acredito que deu certo

--- HeliumAtom/Exercicio51e.py
+++ HeliumAtom/Exercicio51e.py
@@ -12,9 +12,9 @@ def func(x, a, b, c):
 
 
 r_range = [0, 20]
-Eigenvalue_range = [-5, 0.]
+Eigenvalue_range = [-1, 0.]
 
-KS_int_max = 100
+KS_int_max = 1
 
 Eigenvalue_tol = 0.0001
 u0_tol= 0.0001
@@ -35,8 +35,8 @@ u0 = np.zeros(np.size(j_max))
 for i in range(0,int(np.size(u0))):
     print(i)
     print(delta[i]*j_max[i])
-    u0[i] = pydft.hydrogenatom(r_range, Eigenvalue_range, KS_int_max, Eigenvalue_tol, u0_tol,
-                  Uniform_Numerov, h, j_max[i], delta[i], write_data)
+    u0[i] = np.abs(pydft.hydrogenatom(r_range, Eigenvalue_range, KS_int_max, Eigenvalue_tol, u0_tol,
+                  Uniform_Numerov, h, j_max[i], delta[i], write_data))
 
 #%% Fitting
 popt, pcov = curve_fit(func, j_max, u0)
@@ -46,11 +46,11 @@ popt, pcov = curve_fit(func, j_max, u0)
 plt.figure(figsize=(16,9))
 plt.title('Termo de fronteira $u_0$ vs $N$', fontsize=20)
 plt.plot(j_max, u0, label='Calculado', linewidth=4)
-plt.plot(j_max, 1/(j_max), label='$1/N^4$', linewidth=4)
+# plt.plot(j_max, 1/(j_max), label='$1/N^4$', linewidth=4)
 plt.plot(j_max, func(j_max, *popt), '-.', color='r',
           label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt), linewidth = 4)
 plt.xlabel('$N=j_{max}$', fontsize=18)
-plt.ylabel('$u_0$', fontsize=18)
+plt.ylabel('$|u_0|$', fontsize=18)
 plt.xlim(j_max[0],j_max[-1])
 
 # plt.yscale('log')
